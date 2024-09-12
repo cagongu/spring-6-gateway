a750b8e81649508a963bb99127a4d17b5cd98ba4
cagongu <hophucthai9@gmail.com>
2024-09-12 15:58:25 +0700

    refactor conf

--- src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.actuate.autoconfigure.security.reactive.Endpoint
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -25,15 +26,12 @@ public class SpringSecConfig {
 
     @Bean
     @Order(2)
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(withDefaults())
-                );
-        return http.build();
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
 
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return http.build();
     }
 }
